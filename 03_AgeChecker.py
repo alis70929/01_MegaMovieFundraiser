a9ce4a5e937fe5a9d33b8717a3e0a8297c679f4a
alis70929 <60583162+alis70929@users.noreply.github.com>
2021-02-10 14:55:14 +1300

    updated AgeChecker

--- 03_AgeChecker.py
+++ 03_AgeChecker.py
@@ -9,3 +9,23 @@ def intcheck(question,low = None,high = None):
         error = "Please enter a whole number equal to or below {} ".format(high)
     else: # Error message if no variables are given
         error = "please enter a whole number"
+
+    while not valid:
+        try:
+            # Gets user input
+            response = int(input(question.format(low, high)))
+            # Checks number is not too low
+            if low is not None and response < low:
+                print(error)  # If its too low  display error
+                continue
+            # Checks number is not too high
+            if high is not None and response > high:
+                print(error)  # If it is too high print error
+                continue
+
+            return response
+        # If input is not a number or is a decimal then display error
+        except ValueError:
+            print(error)
+            print()
+
