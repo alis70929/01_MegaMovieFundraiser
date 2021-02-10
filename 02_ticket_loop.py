e0b10b16823b6b6c1fd96eccaedd6d735d6575ec
alis70929 <60583162+alis70929@users.noreply.github.com>
2021-02-10 14:06:49 +1300

    Finsihed first iteration of ticket loop

--- 02_ticket_loop.py
+++ 02_ticket_loop.py
@@ -4,10 +4,12 @@ count = 0
 max_tickets = 5
 
 while name != "xxx" and count < max_tickets:
+    print("You have {} tickets left".format(max_tickets-count))
     name = input("Name:")
     count += 1
 
 if count < max_tickets:
+    print("You have sold {}".format((count)))
     print("There are {} tickets left".format(max_tickets-count))
 else:
     print("All tickets have been sold")
