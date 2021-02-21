ce9b33bbcdc2037593a5e76c192c911fabbfb863
alis70929 <60583162+alis70929@users.noreply.github.com>
2021-02-22 10:54:34 +1300

    Finished checkpoint 3, and started string analyser

--- 06_StringCheckerV4.py
+++ 06_StringCheckerV4.py
@@ -2,7 +2,7 @@
 def string_checker(choice, options):
     for var_list in options:
 
-        if choice in var_list:
+        if choice.lower() in var_list:
             chosen = var_list[0].title()
             is_valid = "yes"
             break
@@ -12,6 +12,7 @@ def string_checker(choice, options):
     if is_valid == "yes":
         return chosen
     else:
+        print("Error: Please enter a valid choice")
         return "invalid choice"
 
 valid_snacks = [
@@ -32,11 +33,13 @@ snack_ok = ""
 snack = ""
 
 chosen_snacks = []
+check_snack = "invalid choice"
 while check_snack == "invalid choice":
     want_snack = input("Do you want to buy a snack")
     check_snack = string_checker(want_snack, yes_no_list)
 
-if check_snack == "yes":
+
+if check_snack == "Yes":
 
     desired_snack = ""
     while desired_snack != "xxx":
@@ -47,8 +50,19 @@ if check_snack == "yes":
             break
 
         snack_chosen = string_checker(desired_snack, valid_snacks)
-        print("Snack Chosen: {}".format(snack_chosen))
+        if snack_chosen != "invalid choice":
+            print("Snack Chosen: {}".format(snack_chosen))
 
-        if()
+        if snack_chosen != "xxx" and snack_chosen != "invalid choice":
+            chosen_snacks.append(snack_chosen)
+
+print()
+if len(chosen_snacks) == 0:
+    print("Snacks Ordered: None")
+else:
+    print("Snacks Ordered: ")
+
+    for item in chosen_snacks:
+        print(item)
 
 
