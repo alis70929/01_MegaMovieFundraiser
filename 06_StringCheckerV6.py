d279b9a8fca4d16f2153804444f249a338cbab1a
alis70929 <60583162+alis70929@users.noreply.github.com>
2021-02-23 09:37:59 +1300

    finsihed adding string analyser to string checker making a complete snack analyser.

---
+++ 06_StringCheckerV6.py
@@ -0,0 +1,92 @@
+import re
+
+
+def string_checker(choice, options):
+    for var_list in options:
+
+        if choice.lower() in var_list:
+            chosen = var_list[0].title()
+            is_valid = "yes"
+            break
+        else:
+            is_valid = "no"
+
+    if is_valid == "yes":
+        return chosen
+    else:
+        print("Error: Please enter a valid choice")
+        return "invalid choice"
+
+number_regex = "^[1-9]"
+
+valid_snacks = [
+
+    ["popcorn", "p", "corn", "a"],
+    ["M&M's", "m&m's", "mms", "m", "b"],
+    ["pita chips", "chips", "pc", "pita", "c"],
+    ["water", "w", "d"],
+    ["orange juice", "oj"]
+
+]
+
+yes_no_list = [
+        ["yes", "y"],
+        ["no", "n"]
+    ]
+
+snack_ok = ""
+snack = ""
+
+chosen_snacks = []
+check_snack = "invalid choice"
+while check_snack == "invalid choice":
+    want_snack = input("Do you want to buy a snack")
+    check_snack = string_checker(want_snack, yes_no_list)
+
+
+
+if check_snack == "Yes":
+
+    desired_snack = ""
+    while desired_snack != "xxx":
+
+        desired_snack = input("Snack: ").lower()
+
+        if desired_snack == "xxx":
+            break
+
+        if re.match(number_regex, desired_snack):
+            amount = int(desired_snack[0])
+            desired_snack = desired_snack[1:]
+
+        else:
+            amount = 1
+
+        desired_snack = desired_snack.strip()
+
+        snack_chosen = string_checker(desired_snack, valid_snacks)
+
+        if amount >= 5:
+            print("Sorry we have a four snack maximum")
+            snack_chosen = "invalid choice"
+
+        if snack_chosen != "invalid choice":
+            print("Snack Chosen:{} {}".format(amount,snack_chosen))
+
+
+
+        amount_snack = "{} {}".format(amount,snack_chosen)
+
+        if snack_chosen != "xxx" and snack_chosen != "invalid choice":
+            chosen_snacks.append(amount_snack)
+
+print()
+if len(chosen_snacks) == 0:
+    print("Snacks Ordered: None")
+else:
+    print("Snacks Ordered: ")
+
+    for item in chosen_snacks:
+        print(item)
+
+
