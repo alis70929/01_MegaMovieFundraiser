a53539bc0dc127dca81e9be9fdb98b93c821d393
alis70929 <60583162+alis70929@users.noreply.github.com>
2021-02-23 09:17:20 +1300

    finished iteration 1 of string analyser
    First time using regular expression

--- 07_string_analyser.py
+++ 07_string_analyser.py
@@ -1,3 +1,29 @@
 import re
 
-test strings
+test_strings =\
+    [
+        "Popcorn",
+        "2 pc",
+        "1.50J",
+        "40J"
+    ]
+
+for item in test_strings:
+
+    # regular expression to find if item starts with a number
+    number_regex = "^[1-9]"
+
+    if re.match(number_regex,item):
+        amount = int(item[0])
+        desired_snack = item[1:]
+
+    else:
+        amount = 1
+        desired_snack = item
+
+    desired_snack = desired_snack.strip()
+
+    print("Amount: ",amount)
+    print("Snack: ",desired_snack)
+    print("Length of snack",len(desired_snack))
+
